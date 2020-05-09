3da11371d768e3248424de196cc84efefda071d3
moinulibr <moinulibr@gmail.com>
2020-05-09 10:20:00 +0600

    Modify Product Resource details

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -14,6 +14,6 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return parent::toArray($request); 
     }
 }
